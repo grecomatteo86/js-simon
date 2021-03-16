a245f9133e346dd0e39c04ba589f803f7c3527f2
Matteo <grecomatteo86@gmail.com>
2021-03-16 20:27:25 +0100

    fixes

--- js/script.js
+++ js/script.js
@@ -6,30 +6,105 @@ Dopo che sono stati inseriti i 5 numeri,
 il software dice quanti e quali dei numeri da indovinare
 sono stati individuati. */
 
+
 // 1.
 var arrayCpu = [1,2,3,4,5];
 alert(arrayCpu);
 
+
 // 2.
 var arrayUtente = [];
+
 
 // 3.
 setTimeout(function () {
 
   for ( i = 1; i <= 5; i++) {
 
-    var numeriUtente = prompt('inserisci i numeri');
+    var numeriUtente = parseInt(prompt('inserisci i numeri'));
 
-    // 4.
     arrayUtente.push(numeriUtente);
 
-    // 5.
-    if () {
+  }
 
+  console.log(arrayUtente);
+
+/*
+  var numeriInseriti = [];
+
+  setTimeout(function () {
+
+    for (var i = 0; i < numeriSimon.length; i++) {
+
+      var num = parseInt(prompt("Inserisci qui i numeri di prima, uno alla volta:"));
+      numeriInseriti.push(num);
+    }
+
+  console.log("Questi sono i numeri che hai scritto tu: " + numeriInseriti)*/
+
+
+}, 3000);
+
+
+
+
+/*
+// FUNZIONI GENERICHE
+
+// Randomizer
+function randomizer(min, max){
+  return Math.floor(Math.random()*(max - min + 1) + min);
+}
+
+
+
+//PROGRAMMA
+
+// Creo l'array dei 5 numeri casuali con un croccante ciclo For
+
+var numeriSimon = [];
+
+for (var i = 0; i < 5; i++) {
+ numeriSimon.push(randomizer(1,40));
+}
+
+console.log("Questi sono i numeri generati casualmente " + numeriSimon);
+
+// Stampo l'alert con i numeri da ricordare:
+
+alert("Ricorda bene i numeri " + numeriSimon);
+
+// Definisco array numeri inseriti; uso setTimeOut per inserire una funzione con delay di 30 secondi dall'alert.
+//All'interno uso un ciclo for lungo quanto l'array numeri simon e chiedo x prompt; i numeri inseriti nel prompt sono salvati in una variabile
+// E infine pushati in un array numeriInseriti.
+
+var numeriInseriti = [];
+
+setTimeout(function () {
+
+  for (var i = 0; i < numeriSimon.length; i++) {
+
+    var num = parseInt(prompt("Inserisci qui i numeri di prima, uno alla volta:"));
+    numeriInseriti.push(num);
+  }
+
+console.log("Questi sono i numeri che hai scritto tu: " + numeriInseriti)
+
+  // Confronto i due array attraversandoli con un ciclo for e usando il metodo includes per ogni elemento del primo nei confronti del secondo.
+  //In questo modo definisco il punteggio e i numeri indovinati.
+
+  var numeriIndovinati = [];
+  var punteggio = 0;
+
+  for (var i = 0; i < numeriInseriti.length; i++) {
+
+    if (numeriSimon.includes(numeriInseriti[i])) {
+      numeriIndovinati.push(numeriInseriti[i]);
     }
 
   }
 
-  alert('hai finito');
+  alert("I numeri che hai indovinato sono: " + numeriIndovinati + ". Il tuo punteggio è: " + numeriIndovinati.length);
+  console.log("Hai indovinato i numeri: " + numeriIndovinati + "; il tuo punteggio è: " + numeriIndovinati.length + " su " + numeriSimon.length + " punti massimi totalizzabili.");
 
-}, 3000);
+}, 30000);*/
